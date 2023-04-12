dfece0bee1baf44f2db10f40edf736404636998c
Douglas Malnati <douglas.malnati@gmail.com>
2023-04-12 19:21:48 -0400

    ChannelSearch select band

--- js/WsprLiveQuerier.js
+++ js/WsprLiveQuerier.js
@@ -86,56 +86,76 @@ export class WsprLiveQuerier
         return retVal;
     }
 
-    async GetPossibleBalloonTelemetryU4B()
+    GetPossibleBalloonTelemetryU4BQuery(band)
     {
+        band = parseInt(band);
 
+        let band__dialFreq = new Map();
+        band__dialFreq.set(160,  1836600);
+        band__dialFreq.set(80,   3592600);
+        band__dialFreq.set(60,   5287200);
+        band__dialFreq.set(40,   7038600);
+        // band__dialFreq.set(32,  10138700);
+        band__dialFreq.set(20,  14095600);
+        band__dialFreq.set(17,  18104600);
+        band__dialFreq.set(15,  21094600);
+        band__dialFreq.set(12,  24924600);
+        band__dialFreq.set(10,  28124600);
+        band__dialFreq.set(6,   50293000);
+        band__dialFreq.set(2,  144488500);
 
+        let band__dbBand = new Map();
+        band__dbBand.set(160,  1);
+        band__dbBand.set(80,   3);
+        band__dbBand.set(60,   5);
+        band__dbBand.set(40,   7);
+        // band__dbBand.set(32,  10138700);
+        band__dbBand.set(20,  14);
+        band__dbBand.set(17,  18);
+        band__dbBand.set(15,  21);
+        band__dbBand.set(12,  24);
+        band__dbBand.set(10,  28);
+        band__dbBand.set(6,   50);
+        band__dbBand.set(2,  70);
+
+        if (band__dialFreq.has(band) == false)
+        {
+            band = 20;
+        }
+
+        let dialFreq = band__dialFreq.get(band);
+        let freqFloor = (dialFreq + 1500 - 100);
+
+        let dbBand = band__dbBand.get(band);
 
         let query = `
 select
     round(avg(frequency)) as freqAvg
   , substring(tx_sign, 1, 1) as id1
   , substring(tx_sign, 3, 1) as id3
-  , toInt8((freqAvg - 14097000) / 40) + 1 as freqBand
+  , toInt8((freqAvg - ${freqFloor}) / 40) + 1 as freqBand
   , toMinute(time) % 10 as min
-  , count(*)
+  , count(*) as count
 from wspr.rx
 
 where
       time >= '2023-04-01'
-  and band = 14
+  and band = ${dbBand}
   and match(tx_sign,'^[01Q].[0-9]') = 1
 
 group by (id1, id3, min)
 order by (id1, id3, freqBand, min)
 `;
 
+        return query;
+    }
+
+    async GetPossibleBalloonTelemetryU4B(band)
+    {
+        let query = this.GetPossibleBalloonTelemetryU4BQuery(band);
+        
         return this.DoQueryReturnDataTable(query);
     }
-
-
-    async GetBalloonDataThisMonth()
-    {
-        let query = `
-select
-      tx_sign
-    , time
-    , frequency
-
-from wspr.rx
-
-where
-        time >= '2023-04-01'
-    and band = 14
-    and match(tx_sign,'^[01Q].[0-9]') = 1
-
-order by time
-`;
-
-        return this.DoQuery(query);
-    }
-
-
 }
 
 
