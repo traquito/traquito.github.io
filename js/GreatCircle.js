b8f4a085701cc3a1610d9ab89422c0ecb9fb5402
traquito <douglas.malnati+traquito@gmail.com>
2023-05-26 14:29:35 -0400

    update GreatCircle library to be module importable

--- js/GreatCircle.js
+++ js/GreatCircle.js
@@ -1,22 +1,24 @@
-var GreatCircle = {
+// Adapted from https://github.com/mwgg/GreatCircle
+
+export let GreatCircle = {
 
     validateRadius: function(unit) {
-        var r = {'M': 6371009, 'KM': 6371.009, 'MI': 3958.761, 'NM': 3440.070, 'YD': 6967420, 'FT': 20902260};
+        let r = {'M': 6371009, 'KM': 6371.009, 'MI': 3958.761, 'NM': 3440.070, 'YD': 6967420, 'FT': 20902260};
         if ( unit in r ) return r[unit];
         else return unit;
     },
 
     distance: function(lat1, lon1, lat2, lon2, unit) {
         if ( unit === undefined ) unit = 'KM';
-        var r = this.validateRadius(unit); 
+        let r = this.validateRadius(unit); 
         lat1 *= Math.PI / 180;
         lon1 *= Math.PI / 180;
         lat2 *= Math.PI / 180;
         lon2 *= Math.PI / 180;
-        var lonDelta = lon2 - lon1;
-        var a = Math.pow(Math.cos(lat2) * Math.sin(lonDelta) , 2) + Math.pow(Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(lonDelta) , 2);
-        var b = Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lonDelta);
-        var angle = Math.atan2(Math.sqrt(a) , b);
+        let lonDelta = lon2 - lon1;
+        let a = Math.pow(Math.cos(lat2) * Math.sin(lonDelta) , 2) + Math.pow(Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(lonDelta) , 2);
+        let b = Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lonDelta);
+        let angle = Math.atan2(Math.sqrt(a) , b);
         
         return angle * r;
     },
@@ -26,10 +28,10 @@ var GreatCircle = {
         lon1 *= Math.PI / 180;
         lat2 *= Math.PI / 180;
         lon2 *= Math.PI / 180;
-        var lonDelta = lon2 - lon1;
-        var y = Math.sin(lonDelta) * Math.cos(lat2);
-        var x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(lonDelta);
-        var brng = Math.atan2(y, x);
+        let lonDelta = lon2 - lon1;
+        let y = Math.sin(lonDelta) * Math.cos(lat2);
+        let x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(lonDelta);
+        let brng = Math.atan2(y, x);
         brng = brng * (180 / Math.PI);
         
         if ( brng < 0 ) { brng += 360; }
@@ -39,11 +41,11 @@ var GreatCircle = {
     
     destination: function(lat1, lon1, brng, dt, unit) {
         if ( unit === undefined ) unit = 'KM';
-        var r = this.validateRadius(unit);
+        let r = this.validateRadius(unit);
         lat1 *= Math.PI / 180;
         lon1 *= Math.PI / 180;
-        var lat3 = Math.asin(Math.sin(lat1) * Math.cos(dt / r) + Math.cos(lat1) * Math.sin(dt / r) * Math.cos( brng * Math.PI / 180 ));
-        var lon3 = lon1 + Math.atan2(Math.sin( brng * Math.PI / 180 ) * Math.sin(dt / r) * Math.cos(lat1) , Math.cos(dt / r) - Math.sin(lat1) * Math.sin(lat3));
+        let lat3 = Math.asin(Math.sin(lat1) * Math.cos(dt / r) + Math.cos(lat1) * Math.sin(dt / r) * Math.cos( brng * Math.PI / 180 ));
+        let lon3 = lon1 + Math.atan2(Math.sin( brng * Math.PI / 180 ) * Math.sin(dt / r) * Math.cos(lat1) , Math.cos(dt / r) - Math.sin(lat1) * Math.sin(lat3));
         
         return {
             'LAT': lat3 * 180 / Math.PI,
@@ -51,10 +53,4 @@ var GreatCircle = {
         };
     }
 
-}
-
-if (typeof module != 'undefined' && module.exports) {
-    module.exports = GreatCircle;
-} else {
-    window['GreatCircle'] = GreatCircle;
-}
+};
