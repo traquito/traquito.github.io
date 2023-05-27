33e14f9ba0560e66136e933f4a6f724f302768d1
traquito <douglas.malnati+traquito@gmail.com>
2023-05-27 16:12:44 -0400

    Timeline.js

---
+++ js/Timeline.js
@@ -0,0 +1,55 @@
+import * as utl from '/js/Utl.js';
+
+
+export class Timeline
+{
+    constructor()
+    {
+        this.Reset();
+    }
+
+    Reset()
+    {
+        this.eventList = [];
+        this.longestStr = 0;
+    }
+
+    Event(name)
+    {
+        this.eventList.push({
+            name: name,
+            time: performance.now(),
+        });
+
+        if (name.length > this.longestStr)
+        {
+            this.longestStr = name.length;
+        }
+    }
+
+    Report(msg)
+    {
+        if (msg)
+        {
+            console.log(`Timeline report (${msg}):`);
+        }
+        else
+        {
+            console.log("Timeline report:");
+        }
+
+        // build table to output
+        let objList = [];
+        for (let i = 1; i < this.eventList.length; ++i)
+        {
+            objList.push({
+                from: this.eventList[i - 1].name,
+                to  : this.eventList[i - 0].name,
+                diffMs: utl.Commas(Math.round(this.eventList[i - 0].time - this.eventList[i - 1].time)),
+            })
+        }
+
+        console.table(objList);
+    }
+}
+
